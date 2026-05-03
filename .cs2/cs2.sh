21087366f30a689a626e7f80dd889ac1f063a286
MichaelDeets <michael@deets.sh>
2026-05-03 12:00:53 +0100

    updated configs

--- .cs2/cs2.sh
+++ .cs2/cs2.sh
@@ -6,14 +6,14 @@ export LD_LIBRARY_PATH="${GAMEROOT}"/bin/linuxsteamrt64:$LD_LIBRARY_PATH
 export ENABLE_PATHMATCH=1
 
 # custom envvars
-export RADV_DEBUG=nogpl
+#export RADV_DEBUG=nogpl
 export AMD_TEX_ANISO="0"
 export ENABLE_VKBASALT="1"
 export LD_PRELOAD="/usr/lib64/libSDL3.so.0"
 export MANGOHUD="1"
 export PULSE_LATENCY_MSEC="6"
 export RADV_TEX_ANISO="0"
-export SDL_VIDEO_DRIVER=wayland
+#export SDL_VIDEO_DRIVER=wayland
 export VKBASALT_LOG_LEVEL="none"
 export AMD_USERQ=1
 
